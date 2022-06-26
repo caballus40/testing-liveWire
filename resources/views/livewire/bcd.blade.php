5f63b64fa790ea0d68016d6768a468f198872693
caballus40 <26ridwanfirdaus@gmail.com>
2022-06-26 09:17:11 +0700

    init, install livewire,

---
+++ resources/views/livewire/bcd.blade.php
@@ -0,0 +1,3 @@
+<div>
+    waswas
+    </div>
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <button class="btn btn-primary" wire:click.prevent="decrease">-</button>
+    <div>{{ $counter }}</div>
+    <button class="btn btn-primary" wire:click.prevent="increase">+</button>
+</div>
